a859d06c50a1f99b05f0d0f9e6ce3e3c37667697
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-05-11 20:53:01 +0530

    Bumped version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='tendril-config',
-    version='0.1.6',
+    version='0.1.7',
     description="Tendril Config Infrastructure",
     long_description=readme,
     author="Chintalagiri Shashank",
