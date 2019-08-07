5c543c9ab2dd65eb88a6f529a91fa20156da40ee
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-08-07 13:48:45 +0530

    Minor docstring fixes

--- src/tendril/config/legacy.py
+++ src/tendril/config/legacy.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 The Config Module (:mod:`tendril.config.legacy`)
-===============================================
+================================================
 
 This module provides the core configuration for Tendril.
 
