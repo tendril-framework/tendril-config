875f2b7df9f7e3a661ac7276a24a8a2ba26a9a45
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-11-22 12:20:05 +0530

    Refactor config for kubernetes deployments and shared OS deployments

--- src/tendril/config/core.py
+++ src/tendril/config/core.py
@@ -20,59 +20,13 @@ Core Configuration Constants
 """
 
 import os
-from tendril.utils.config import ConfigConstant
+from tendril.utils.config import install_config
 from tendril.utils import log
 logger = log.get_logger(__name__, log.DEFAULT)
 
 depends = []
 
 
-config_constants_basic = [
-    ConfigConstant(
-        'INSTANCE_ROOT',
-        "os.path.join(os.path.expanduser('~'), '.tendril')",
-        "Path to the tendril instance root. Can be redirected if necessary"
-        "with a file named ``redirect`` in this folder."
-    )
-]
-
-
-config_constants_redirected = [
-    ConfigConstant(
-        'INSTANCE_CONFIG_FILE',
-        "os.path.join(INSTANCE_ROOT, 'instance_config.py')",
-        'Path to the tendril instance configuration.'
-    ),
-    ConfigConstant(
-        'LOCAL_CONFIG_FILE',
-        "os.path.join(INSTANCE_ROOT, 'local_config_overrides.py')",
-        'Path to local overrides to the instance configuration.'
-    ),
-]
-
-config_constants_external = [
-    ConfigConstant(
-        'EXTERNAL_CONFIG_SOURCES',
-        "os.path.join(INSTANCE_ROOT, 'external_configs.yaml')",
-        "Path to a yaml definition file mapping to external config sources."
-    )
-]
-
-
 def load(manager):
     logger.debug("Loading {0}".format(__name__))
-    manager.load_elements(config_constants_basic,
-                          doc="Tendril Default Instance Root")
-
-    if os.path.exists(os.path.join(manager.INSTANCE_ROOT, 'redirect')):
-        logger.info("Found instance redirect")
-        with open(os.path.join(manager.INSTANCE_ROOT, 'redirect'), 'r') as f:
-            manager.INSTANCE_ROOT = f.read().strip()
-
-    manager.load_elements(config_constants_redirected,
-                          doc="Tendril Configuration Paths")
-
-    manager.load_elements(config_constants_external,
-                          doc="External Configuration Sources")
-
-    manager.load_config_files()
+    install_config(manager, os.environ.get('TENDRIL_INSTANCE_NAME', 'tendril'))
