6e31581018708d2510cfd2698cad2baba9de7422
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-03-29 23:32:58 +0530

    Add log controls to produce shorter log outputs.

--- src/tendril/config/log.py
+++ src/tendril/config/log.py
@@ -36,13 +36,58 @@ config_elements_log = [
     ),
     ConfigOption(
         'JSON_LOGS', "''",
-        "Whether to use JSON logging.",
+        "Whether to use JSON logging. Applies only to the logfile, and not to the "
+        "logs displayed on the console.",
         parser=bool
     ),
     ConfigOption(
         'LOG_PATH', "os.path.join('/var/log', 'tendril', '{}.log'.format(INSTANCE_NAME))",
         "Full path (including filename) to write logs to.",
     ),
+    ConfigOption(
+        'LOG_COMPACT_TS', 'True',
+        "Whether to shorten the timestamp. See LOG_COMPACT_TS_READABLE.",
+        parser=bool
+    ),
+    ConfigOption(
+        'LOG_COMPACT_TS_READABLE', 'True',
+        "Whether to use a readable format for the compact timestamp. Does not show year and "
+        "uses minimal punctuation to keep it short yet readable. If False, shows UNIX "
+        "timestamp only, which is even shorter.",
+        parser=bool
+    ),
+    ConfigOption(
+        'LOG_COMPACT_SOURCE', 'True',
+        "Whether to shorten the source module name.",
+        parser=bool
+    ),
+    ConfigOption(
+        'LOG_COMPACT_SOURCE_MAXLEN', '18',
+        "Target max length for the source module. Actual length might yet be longer.",
+        parser=int
+    ),
+    ConfigOption(
+        'LOG_COMPACT_LEVEL', 'True',
+        "Whether to shorten the level name to one letter.",
+        parser=bool
+    ),
+    ConfigOption(
+        'LOG_COMPACT_LEVEL_ICON', 'False',
+        "Whether to show the level icon instead of the level name. Use only for regular, full "
+        "featured terminals. The single letter representation is usually better.",
+        parser=bool
+    ),
+    ConfigOption(
+        'LOG_INCLUDE_HOSTNAME', 'False',
+        "Whether to include hostname in the log. This is provided to help deal with logs "
+        "from multiple replicas or components running in a containerized environment.",
+        parser=bool
+    ),
+    ConfigOption(
+        'LOG_HOSTNAME_PREFIX', '""',
+        "If set, this prefix is stripped from the hostname and only the remainder is "
+        "included in the log message."
+    )
 ]
 
 
@@ -52,4 +97,3 @@ def load(manager):
                           doc="Tendril Server Logging Configuration")
     from tendril.utils import log
     log.apply_config(manager)
-
